337333bc38f2ca2349e2841f9fb6dc2b332c3ad9
Capppe <casper.nordstrom@ha.ax>
2024-09-25 21:50:30 +0300

    Added option to parse a single variant

--- src/dbus_utils.rs
+++ src/dbus_utils.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use dbus::{
-    arg::{PropMap, Variant},
+    arg::{PropMap, RefArg, Variant},
     blocking::{Proxy, SyncConnection},
 };
 
@@ -22,6 +22,16 @@ pub fn parse_propmap<'a>(props: &PropMap) -> HashMap<String, String> {
             (key.clone(), value_as_string.trim().to_owned())
         })
         .collect()
+}
+
+pub fn parse_variant<R: 'static>(variant: &Variant<Box<dyn RefArg>>) -> Result<&R, String> {
+    let ref_arg = variant.0.as_ref();
+
+    if let Some(r) = ref_arg.as_any().downcast_ref::<R>() {
+        Ok(r)
+    } else {
+        Err(format!("Failed to convert {:?} to specified type", variant))
+    }
 }
 
 fn variant_to_string<T: std::fmt::Debug>(variant: &Variant<T>) -> String {
--- tests/v2/root/object_manager_tests.rs
+++ tests/v2/root/object_manager_tests.rs
@@ -2,10 +2,16 @@ extern crate bt_wrapper;
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
+    use std::{
+        any::{Any, TypeId},
+        collections::HashMap,
+    };
 
-    use bt_wrapper::{root::object_manager::ObjectManager, DBusItem};
-    use dbus::{arg::PropMap, Path};
+    use bt_wrapper::{dbus_utils, root::object_manager::ObjectManager, DBusItem};
+    use dbus::{
+        arg::{PropMap, RefArg, Variant},
+        Path,
+    };
 
     #[tokio::test]
     async fn test_method_get_managed_objects() {
@@ -16,6 +22,24 @@ mod tests {
         println!("Res: {:?}", res);
 
         assert!(!res.is_empty())
+    }
+
+    #[test]
+    fn test_method_parse_variant() {
+        let str: Variant<Box<dyn RefArg>> = Variant(Box::new("Hej".to_string()));
+        let int: Variant<Box<dyn RefArg>> = Variant(Box::new(12 as i32));
+        let float: Variant<Box<dyn RefArg>> = Variant(Box::new(12.0 as u32));
+        let bool: Variant<Box<dyn RefArg>> = Variant(Box::new(true));
+
+        let p_str = dbus_utils::parse_variant::<String>(&str).expect("Failed");
+        let p_int = dbus_utils::parse_variant::<i32>(&int).expect("Failed");
+        let p_float = dbus_utils::parse_variant::<u32>(&float).expect("Failed");
+        let p_bool = dbus_utils::parse_variant::<bool>(&bool).expect("Failed");
+
+        assert!(p_str.type_id() == TypeId::of::<String>());
+        assert!(p_int.type_id() == TypeId::of::<i32>());
+        assert!(p_float.type_id() == TypeId::of::<u32>());
+        assert!(p_bool.type_id() == TypeId::of::<bool>())
     }
 
     #[tokio::test]
